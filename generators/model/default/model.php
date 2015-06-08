cf5777a447e5ff6b90bd8db928d499992e7f242e
mj4444ru <mj4444@mail.ru>
2015-06-08 18:25:27 +0500

    fix

---
+++ generators/model/default/model.php
@@ -0,0 +1,2 @@
+<?php
+require(__DIR__."/../../../../../yiisoft/yii2-gii/generators/model/default/model.php");
---
+++ generators/model/form.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    document.getElementById('mjDoubleModel-generator').setAttribute('id', 'model-generator');
+</script>
+<?php
+$form->id = 'model-generator';
+$form->options['id'] = 'model-generator';
+require(__DIR__."/../../../../yiisoft/yii2-gii/generators/model/form.php");
