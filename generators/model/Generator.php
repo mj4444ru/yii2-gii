cb50cfa6b528c191249b9557b23cb55212f3396e
mj4444ru <mj4444@mail.ru>
2015-06-08 18:09:12 +0500

    fix

--- generators/model/Generator.php
+++ generators/model/Generator.php
@@ -4,4 +4,22 @@ namespace mj4444\yii2gii\generators\model;
 
 class Generator extends \yii\gii\generators\model\Generator
 {
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+//xxx;
+//var_dump($this);
+//die('2');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return 'mj4444 Model Generator';
+    }
 }
